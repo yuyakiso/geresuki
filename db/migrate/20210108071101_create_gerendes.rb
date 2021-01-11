c82536e41be31e4edba6afa1b1e3f153c7bacb7c
yuyakiso <yuya2226@gmail.com>
2021-01-11 12:23:40 +0900

    データベース作成

---
+++ db/migrate/20210108071101_create_gerendes.rb
@@ -0,0 +1,9 @@
+class CreateGerendes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :gerendes do |t|
+      t.string  :name, null:false
+      t.integer :prefecture_id, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_030910) do
+ActiveRecord::Schema.define(version: 2021_01_08_071101) do
+
+  create_table "gerendes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "prefecture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
