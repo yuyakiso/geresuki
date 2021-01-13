dcd2f28211eea5b56a619adfdd066fad657f94a9
yuyakiso <yuya2226@gmail.com>
2021-01-13 17:03:43 +0900

    コントローラーの調整

--- app/controllers/gerendes_controller.rb
+++ app/controllers/gerendes_controller.rb
@@ -1,10 +1,20 @@
 class GerendesController < ApplicationController
+  before_action :set_gerende, only: [:show]
   def index
-    @gerendes = Gerende.all
+    @gerendes = Gerende.all.page(params[:page])
   end
 
   def search
     @gerendes = Gerende.search(params)
   end
 
+  def show
+    @comment = Comment.new
+    @comments = @gerende.comments.includes(:user)
+  end
+
+  private
+  def set_gerende
+    @gerende = Gerende.find(params[:id])
+  end
 end
