292d6cee1827f96c5c9c9cb2f384aceb36e45d43
yuyakiso <yuya2226@gmail.com>
2021-01-13 16:57:46 +0900

    ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "gerendes#index"
-  resources :gerendes, only: [:index] do
+  resources :gerendes, only: [:index, :show] do
+    resources :comments, only: [:create]
     collection do
       get 'search'
     end
