937ea8005a9931c8eb9766202a7a8be9ceb6b8f5
yuyakiso <yuya2226@gmail.com>
2021-01-17 14:25:11 +0900

    コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,16 @@ class CommentsController < ApplicationController
     redirect_to "/gerendes/#{comment.gerende_id}"
   end
 
+  def destroy
+    @gerende = Gerende.find(params[:id])
+    @comment = Comment.find(params[:id])
+    if @comment.user.id != current_user.id
+      redirect_to action: index
+    end
+    @comment.destroy
+    redirect_to @gerende
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text, :image).merge(user_id: current_user.id, gerende_id: params[:gerende_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "gerendes#index"
   resources :gerendes, only: [:index, :show] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
     collection do
       get 'search'
     end
   end
-    
 end
