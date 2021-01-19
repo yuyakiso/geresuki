b751341357d54d0fe0f46b87c22d59340d9ff8cd
yuyakiso <yuya2226@gmail.com>
2021-01-19 14:20:40 +0900

    不要部分削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:index, :search, :show]
 
   private
   def configure_permitted_parameters
