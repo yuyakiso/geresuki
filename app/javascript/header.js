efb044d41ca7df777a8945ceb1b3f6d4db3d71c4
yuyakiso <yuya2226@gmail.com>
2021-01-25 16:09:17 +0900

    フロント実装追加修正

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -4,14 +4,6 @@ function pullDown() {
   const pullDownParents = document.getElementById("pull-down")
   const pullDownChild = document.querySelectorAll(".pull-down-list")
 
-  pullDownButton.addEventListener('mouseover', function(){
-    this.setAttribute("style", "background-color:#FFBEDA;")
-  })
-
-  pullDownButton.addEventListener('mouseout', function(){
-    this.removeAttribute("style", "background-color:#FFBEDA")
-  })
-
   pullDownButton.addEventListener('click', function(){
     if (pullDownParents.getAttribute("style") == "display:block;"){
       pullDownParents.removeAttribute("style","display:block;")
